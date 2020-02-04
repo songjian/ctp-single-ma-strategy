f0233b74955d91f702a2268b87647ffb6bab2842
宋健 <001@codeorder.cn>
2020-02-04 15:59:31 +0800

    MarketDataRecord.h功能和代码合并到MdHandler.h中

--- SubscribeMarketData/MdHandler.h
+++ SubscribeMarketData/MdHandler.h
@@ -1,18 +1,71 @@
 #pragma once
 #include "CustomMdHandler.h"
-#include "MarketDataRecord.h"
-
-extern MarketDataRecord g_mdRecord;
+#include <fstream>
+#include <cassert>
 
 class CMdHandler : public CCustomMdHandler
 {
 public:
-	CMdHandler(CThostFtdcMdApi* pUserMdApi) : CCustomMdHandler(pUserMdApi) {};
+	CMdHandler(CThostFtdcMdApi* pUserMdApi) : CCustomMdHandler(pUserMdApi)
+	{};
+
 	///深度行情通知
 	void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField* pDepthMarketData)
 	{
-		g_mdRecord.update(pDepthMarketData);
-		printf("接收行情,%s,%lf\n", pDepthMarketData->InstrumentID, pDepthMarketData->LastPrice);
+		//写入文件
+		MdToFile(pDepthMarketData);
+
+		//输出信息
+		cout
+			<< "接收行情," << pDepthMarketData->InstrumentID
+			<< ',' << pDepthMarketData->LastPrice
+			<< endl;
 	}
+
+private:
+	//市场深度数据写入文件
+	void MdToFile(CThostFtdcDepthMarketDataField* pDepthMarketData)
+	{
+		using namespace std;
+
+		//dmd字段不能为空
+		assert(pDepthMarketData->InstrumentID != '\0');
+		assert(pDepthMarketData->TradingDay != '\0');
+
+		// 追加写入记录
+		ofstream outfile(FilePath(pDepthMarketData), ios::app);
+
+		// 写入数据
+		outfile
+			<< pDepthMarketData->TradingDay << ','
+			<< pDepthMarketData->ActionDay << ','
+			<< pDepthMarketData->UpdateTime << ','
+			<< pDepthMarketData->UpdateMillisec << ','
+			<< pDepthMarketData->ExchangeID << ','
+			<< pDepthMarketData->InstrumentID << ','
+			<< pDepthMarketData->OpenPrice << ','
+			<< pDepthMarketData->HighestPrice << ','
+			<< pDepthMarketData->LowestPrice << ','
+			<< pDepthMarketData->ClosePrice << ','
+			<< pDepthMarketData->LastPrice << ','
+			<< pDepthMarketData->Volume << ','
+			<< pDepthMarketData->Turnover << endl;
+
+		outfile.close();
+	};
+
+	// 取得输出文件路径
+	string FilePath(CThostFtdcDepthMarketDataField* pDepthMarketData)
+	{
+		return chDmdDir
+			+ pDepthMarketData->InstrumentID
+			+ '_'
+			+ pDepthMarketData->TradingDay
+			+ ".csv";
+	};
+
+private:
+	//深度市场数据存储目录
+	string chDmdDir = "./data/dmd/";
 };
 
--- SubscribeMarketData/main.h
+++ SubscribeMarketData/main.h
@@ -1,11 +1,9 @@
 #pragma once
 #include "MdHandler.h"
-#include "MarketDataRecord.h"
 #include <synchapi.h>
 #include "GetInstrumentID.h"
 
 using namespace std;
 
 HANDLE g_hEvent = CreateEvent(NULL, false, false, NULL);
-MarketDataRecord g_mdRecord;
 vector<string> md_InstrumentID;
